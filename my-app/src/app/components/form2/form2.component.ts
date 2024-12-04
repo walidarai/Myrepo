8c4dffe8886ab1e93b4bb84bae6601d7a01cf67c
walidarai <walid@araiinfotech.com>
2024-12-04 15:55:30 +0530

    added validation

--- my-app/src/app/components/form2/form2.component.ts
+++ my-app/src/app/components/form2/form2.component.ts
@@ -1,6 +1,6 @@
 import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form2',
@@ -12,12 +12,12 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 export class Form2Component {
 
   studentForm = new FormGroup({
-    firstName : new FormControl(),
-    lastName : new FormControl(),
-    userName : new FormControl(),
-    city : new FormControl(),
-    state : new FormControl(),
-    zip : new FormControl(),
+    firstName : new FormControl("",[Validators.required,Validators.minLength(4)]),
+    lastName : new FormControl("",[Validators.required,Validators.minLength(4)]),
+    userName : new FormControl("",[Validators.required,Validators.email]),
+    city : new FormControl("",[Validators.required]),
+    state : new FormControl("",[Validators.required]),
+    zip : new FormControl("",[Validators.required,Validators.minLength(4)]),
 
   })
 
