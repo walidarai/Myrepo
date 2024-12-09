f6179e7697e38e9de6b1df3e0bbf3eb0ee4d66b5
walidarai <walid@araiinfotech.com>
2024-12-09 12:46:40 +0530

    api call get and post

--- my-app/src/app/components/new-employee/new-employee.component.ts
+++ my-app/src/app/components/new-employee/new-employee.component.ts
@@ -1,24 +1,54 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component,OnInit} from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-employee',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-employee.component.html',
   styleUrl: './new-employee.component.css'
 })
-export class NewEmployeeComponent {
+export class NewEmployeeComponent implements OnInit {
 
   userList:any  = [];
+  depList:any  = [];
 
   constructor(private http: HttpClient){
 
   }
 
+  ngOnInit(): void {
+    this.getDep();
+  }
+
   getData(){
     this.http.get("https://jsonplaceholder.typicode.com/users").subscribe((res)=>{
       this.userList = res;
+    })
+  }
+
+  depData:any = {
+    "departmentId": 0,
+    "departmentName": "",
+    "departmentLogo": ""
+  }
+
+  onSave(){
+    this.http.post("https://projectapi.gerasim.in/api/Complaint/AddNewDepartment", this.depData).subscribe((res:any)=>{
+      if(res.result){
+        alert("Data Sent Sucessfully")
+        this.getDep()
+      }else{
+        alert(res.message)
+      }
+    })
+  }
+
+  getDep(){
+    this.http.get("https://projectapi.gerasim.in/api/Complaint/GetParentDepartment").subscribe((res:any)=>{
+      this.depList=res.data;
+      console.log(this.depList)
     })
   }
 
