75c4a417885e92cc690f1efa41d357223f7eaad8
walidarai <walid@araiinfotech.com>
2024-12-24 11:07:29 +0530

    added view-child

--- my-app/src/app/components/view-child/view-child.component.ts
+++ my-app/src/app/components/view-child/view-child.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { AlertComponent } from '../../reusable/alert/alert.component';
 
 @Component({
   selector: 'app-view-child',
   standalone: true,
-  imports: [],
+  imports: [AlertComponent],
   templateUrl: './view-child.component.html',
   styleUrl: './view-child.component.css'
 })
-export class ViewChildComponent {
+export class ViewChildComponent implements AfterContentInit{
+
+  @ViewChild('txt') textBox?:ElementRef
+
+  ngAfterContentInit(): void {
+    
+  }
 
 }
